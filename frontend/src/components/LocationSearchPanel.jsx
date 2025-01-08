ef3821e9130841dff3b0f9324e7e29dd81360c6e
alfardan59 <alfardanarfin500@gmail.com>
2025-01-08 23:23:17 +0530

    Initial Commit

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+      {/* LocationSearchPanel Sample Data */}
+
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] flex items-center justify-center h-9 w-12 rounded-full'><i className='ri-map-pin-fill'></i></h2>
+        <h4 className='font-medium'>24B, Rani Rashmoni Roy Road,South City Mall,Kolkata</h4>
+      </div>
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] flex items-center justify-center h-9 w-12 rounded-full'><i className='ri-map-pin-fill'></i></h2>
+        <h4 className='font-medium'>24B, Rani Rashmoni Roy Road,South City Mall,Kolkata</h4>
+      </div>
+      <div className='flex gap-4 items-center my-4 justify-start'>
+        <h2 className='bg-[#eee] flex items-center justify-center h-9 w-12 rounded-full'><i className='ri-map-pin-fill'></i></h2>
+        <h4 className='font-medium'>24B, Rani Rashmoni Roy Road,South City Mall,Kolkata</h4>
+      </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
